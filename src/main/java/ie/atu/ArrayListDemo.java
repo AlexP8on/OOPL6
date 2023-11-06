34c1e3ee4d2b80a3d5cd7d249220953d2eb08cf4
Alex Paton <G00424185@atu.ie>
2023-11-06 17:28:06 +0000

    Created new java class ArrayListDemo

---
+++ src/main/java/ie/atu/ArrayListDemo.java
@@ -0,0 +1,32 @@
+package ie.atu;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+public class ArrayListDemo {
+
+    ArrayList<Integer> values = new ArrayList<Integer>();
+
+    int indexValue = 0;
+        for(int value: )
+    {
+        System.out.println("Element at index " + indexValue + " : " + value);
+        indexValue++;
+    }
+
+    Scanner myScan = new Scanner(System.in);
+
+        try
+    {
+        System.out.println("Enter a new number: ");
+        anArray[10]= myScan.nextInt();
+
+    }
+
+        catch(ArrayIndexOutOfBoundsException aiob)
+    {
+        System.out.println("We went out of the allocated memory block " + aiob);
+    }
+
+}
+}
+}
