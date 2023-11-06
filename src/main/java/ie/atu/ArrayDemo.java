3f8b58903c2a8ff5a5af7bc06d0a592861deb09a
Alex Paton <G00424185@atu.ie>
2023-11-06 16:56:59 +0000

    initialised Elements

--- src/main/java/ie/atu/ArrayDemo.java
+++ src/main/java/ie/atu/ArrayDemo.java
@@ -5,6 +5,23 @@ public class ArrayDemo {
 
     public static void main(String[] args)
     {
+        //declares an array of integers
+        int[] anArray;
+
+//allocates memory for the 10 integers
+        anArray = new int[10];
+
+//initialize Elements
+        anArray[0] = 100;
+        anArray[1] = 200;
+        anArray[2] = 300;
+        anArray[3] = 400;
+        anArray[4] = 500;
+        anArray[5] = 600;
+        anArray[6] = 700;
+        anArray[7] = 800;
+        anArray[8] = 900;
+        anArray[9] = 1000;
 
     }
 }
